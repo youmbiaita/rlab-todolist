bd91371df8667f46a4805d6b39ac5417d716ab95
Tchakounteaita <164936530+youmbiaita@users.noreply.github.com>
2024-06-16 21:34:38 -0400

    edited date file

--- React-Todo_List/src/data.js
+++ React-Todo_List/src/data.js
@@ -2,121 +2,103 @@ const initialState = [
     {
       "userId": 1,
       "id": 1,
-      "title": "delectus aut autem",
+      "title": "wake up",
       "completed": false
     },
     {
       "userId": 1,
       "id": 2,
-      "title": "quis ut nam facilis et officia qui",
+      "title": "prayer",
       "completed": false
     },
     {
       "userId": 1,
       "id": 3,
-      "title": "fugiat veniam minus",
+      "title": "make my bed",
       "completed": false
     },
     {
       "userId": 1,
       "id": 4,
-      "title": "et porro tempora",
-      "completed": true
-    },
-    {
-      "userId": 1,
-      "id": 5,
-      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "title": "Shower",
       "completed": false
     },
     {
       "userId": 1,
+      "id": 5,
+      "title": "Work out",
+      "completed": true
+    },
+    {
+      "userId": 1,
       "id": 6,
-      "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+      "title": "clean up",
       "completed": false
     },
     {
       "userId": 1,
       "id": 7,
-      "title": "illo expedita consequatur quia in",
+      "title": "Breakfast",
       "completed": false
     },
     {
       "userId": 1,
       "id": 8,
-      "title": "quo adipisci enim quam ut ab",
-      "completed": true
-    },
-    {
-      "userId": 1,
-      "id": 9,
-      "title": "molestiae perspiciatis ipsa",
+      "title": "TV",
       "completed": false
     },
     {
       "userId": 1,
-      "id": 10,
-      "title": "illo est ratione doloremque quia maiores aut",
+      "id": 9,
+      "title": "Sleep",
       "completed": true
     },
     {
       "userId": 1,
+      "id": 10,
+      "title": "Family time",
+      "completed": false
+    },
+    {
+      "userId": 1,
       "id": 11,
-      "title": "vero rerum temporibus dolor",
+      "title": "Studying",
       "completed": true
     },
     {
       "userId": 1,
       "id": 12,
-      "title": "ipsa repellendus fugit nisi",
+      "title": "Rest",
       "completed": true
     },
     {
       "userId": 1,
       "id": 13,
-      "title": "et doloremque nulla",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 14,
-      "title": "repellendus sunt dolores architecto voluptatum",
+      "title": "Dance Club",
       "completed": true
     },
     {
       "userId": 1,
+      "id": 14,
+      "title": "Shower",
+      "completed": false
+    },
+    {
+      "userId": 1,
       "id": 15,
-      "title": "ab voluptatum amet voluptas",
+      "title": "Study",
       "completed": true
     },
     {
       "userId": 1,
       "id": 16,
-      "title": "accusamus eos facilis sint et aut voluptatem",
+      "title": "Prayer",
       "completed": true
     },
     {
       "userId": 1,
       "id": 17,
-      "title": "quo laboriosam deleniti aut qui",
-      "completed": true
-    },
-    {
-      "userId": 1,
-      "id": 18,
-      "title": "dolorum est consequatur ea mollitia in culpa",
-      "completed": false
-    },
-    {
-      "userId": 1,
-      "id": 19,
-      "title": "molestiae ipsa aut voluptatibus pariatur dolor nihil",
-      "completed": true
-    },
-    {
-      "userId": 1,
-      "id": 20,
-      "title": "ullam nobis libero sapiente ad optio sint",
+      "title": "Sleep",
       "completed": true
     }
   ];
